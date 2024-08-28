07d0d6c564f8268e788eb1a92a7b629890833241
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-08-28 16:31:42 +0530

    Location

---
+++ client/src/components/nav/Location.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Location = () => {
+  const locations = ["Bankura", "Kolkata", "Dubai"];
+
+  return (
+    <div className="h-[300px] w-full inset-0 bg-black absolute">
+      <ul className="flex gap-5 items-center justify-center">
+        {locations.map((location, index) => (
+          <li key={index} className="text-white">{location}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Location;
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Location from "./Location";
 
 const Nav = () => {
+  const [locationVisible, setLocationVisible] = useState(false);
+
+  const handleClick = () => {
+    setLocationVisible((prev) => !prev);
+  };
+
   return (
-    <div>
-      <nav className="flex justify-between py-5  px-[15%]">
+    <div className="flex flex-col">
+      <nav className="flex justify-between py-5 px-[15%]">
         <h3>OrrinSanMato</h3>
         <input
           id="inputField"
           type="text"
-          placeholder="Search for resturent"
+          placeholder="Search for restaurant"
           className="w-[30vw] px-4 py-2 border border-white-100 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 transition duration-150 ease-in-out"
         />
         <div className="flex gap-5">
+          <div className="" onClick={handleClick}>
+            Location
+          </div>
           <span>Log in</span>
           <span>Sign up</span>
         </div>
       </nav>
+    <div className=" relative flex items-center justify-center">
+      {locationVisible && <Location />}
+      </div>
     </div>
   );
 };
