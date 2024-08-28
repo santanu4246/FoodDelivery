c599c359a0053b17f1c6d6cd4c7a43e989d6618a
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-08-28 17:00:28 +0530

    Location ui

--- client/src/components/nav/Location.jsx
+++ client/src/components/nav/Location.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { RxCross1 } from "react-icons/rx";
-import { useRestrurant } from "../../store/Restrurants";
 
 const Location = ({ setLocationVisible }) => {
-  const { allLocations } = useRestrurant();
+  const locations = ["Bankura", "Kolkata", "Dubai"];
 
   return (
-    <div className="select-none flex justify-center h-[300px] w-full inset-0 bg-transparent text-black absolute z-10 ">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-20">
       <div className="relative border border-[#00000038] w-[80%] shadow-md rounded-[10px] bg-white">
         <RxCross1
           onClick={() => setLocationVisible(false)}
@@ -16,7 +15,7 @@ const Location = ({ setLocationVisible }) => {
           Select your preferred location
         </h2>
         <ul className="p-[20px] flex gap-5 items-center justify-center">
-          {allLocations.map((location, index) => (
+          {locations.map((location, index) => (
             <li
               onClick={() => setLocationVisible(false)}
               key={index}
