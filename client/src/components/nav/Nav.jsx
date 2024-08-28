1165275d884d2bc6d14864ebfc1c1b081e36ca9c
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-08-28 20:36:30 +0530

    Location ui

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Location from "./Location";
-
+import { CiLocationOn } from "react-icons/ci";
 const Nav = () => {
   const [locationVisible, setLocationVisible] = useState(false);
 
@@ -19,17 +19,20 @@ const Nav = () => {
           className="w-[30vw] px-4 py-2 border border-white-100 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 transition duration-150 ease-in-out"
         />
         <div className="flex gap-5">
-          <div onClick={handleClick} className="cursor-pointer">
+          <div onClick={handleClick} className="cursor-pointer ">
+            <div className="flex items-center">
+          <CiLocationOn />
             Location
+            </div>
           </div>
           <span>Log in</span>
           <span>Sign up</span>
         </div>
       </nav>
       {locationVisible && (
-        <div className="fixed inset-0 z-10">
+        
           <Location setLocationVisible={setLocationVisible} />
-        </div>
+        
       )}
     </div>
   );
