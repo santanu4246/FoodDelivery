d6ab2a73637f4720ad183c2a736819b72759dd09
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-08-28 16:57:46 +0530

    Location ui

--- client/src/components/nav/Location.jsx
+++ client/src/components/nav/Location.jsx
@@ -5,23 +5,19 @@ const Location = ({ setLocationVisible }) => {
   const locations = ["Bankura", "Kolkata", "Dubai"];
 
   return (
-    <div className="flex justify-center h-[300px] w-full inset-0 bg-transparent text-black absolute z-10 ">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-20">
       <div className="relative border border-[#00000038] w-[80%] shadow-md rounded-[10px] bg-white">
         <RxCross1
-          onClick={() => {
-            setLocationVisible(false);
-          }}
+          onClick={() => setLocationVisible(false)}
           className="absolute top-[20px] right-[20px] text-[18px] cursor-pointer"
         />
         <h2 className="text-center text-[15px] font-[500] mt-[1rem]">
-          Select your preferred locaiton
+          Select your preferred location
         </h2>
         <ul className="p-[20px] flex gap-5 items-center justify-center">
           {locations.map((location, index) => (
             <li
-              onClick={() => {
-                setLocationVisible(false);
-              }}
+              onClick={() => setLocationVisible(false)}
               key={index}
               className="px-[1rem] py-[5px] bg-[#e5e5e5] transition-all ease-linear duration-200 hover:bg-[#d2d2d2] rounded-[5px] cursor-pointer"
             >
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Location from "./Location";
 
-
 const Nav = () => {
   const [locationVisible, setLocationVisible] = useState(false);
 
@@ -20,18 +19,18 @@ const Nav = () => {
           className="w-[30vw] px-4 py-2 border border-white-100 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 transition duration-150 ease-in-out"
         />
         <div className="flex gap-5">
-          <div className="" onClick={handleClick}>
+          <div onClick={handleClick} className="cursor-pointer">
             Location
           </div>
           <span>Log in</span>
           <span>Sign up</span>
         </div>
       </nav>
-      <div className=" relative flex items-center justify-center">
-        {locationVisible && (
+      {locationVisible && (
+        <div className="fixed inset-0 z-10">
           <Location setLocationVisible={setLocationVisible} />
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
