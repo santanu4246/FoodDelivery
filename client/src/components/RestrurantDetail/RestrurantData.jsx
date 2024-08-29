3a7223a9ae01bf633e8d8f71aa81f7a27766d13a
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-08-29 21:53:31 +0530

    Resturents Details

--- client/src/components/RestrurantDetail/RestrurantData.jsx
+++ client/src/components/RestrurantDetail/RestrurantData.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAdminAuthentication } from "../../store/Authentication";
+import { FaDirections } from "react-icons/fa";
 
 function RestrurantData() {
   const { id } = useParams();
   const { getRestrurantById } = useAdminAuthentication();
-  const [restrurantData, setRestrurantData] = useState(null);
+  const [restrurantData, setRestrurantData] = useState([]);
+
   useEffect(() => {
     async function getRestrurant() {
       try {
@@ -21,7 +23,49 @@ function RestrurantData() {
   useEffect(() => {
     console.log(restrurantData);
   }, [restrurantData]);
-  return <div>RestrurantData</div>;
+
+  return (
+    <div className="h-screen w-full px-[10%] py-[5%] bg-gradient-to-br bg-white text-black">
+      <div className="w-full max-w-5xl mx-auto bg-white bg-opacity-10 rounded-xl">
+        <div className="h-[300px] w-[490px] overflow-hidden rounded-xl shadow-lg">
+          <img
+            src={restrurantData.image}
+            alt="Restaurant"
+            className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="mt-6">
+          <h2 className="text-4xl font-bold text-black">{restrurantData.name}</h2>
+        </div>
+        
+        <div className="mt-2">
+          {
+            restrurantData.cuisine && restrurantData.cuisine.map((item,index)=>{
+              return <span key={index} className="text-lg font-[500] text-gray-600">{item} {index < restrurantData.cuisine.length - 1 ? ", " : ""}</span>
+            })
+          }
+          
+        </div>
+
+        <div className="mt-2">
+          <span className="text-lg font-[500] text-gray-500">{restrurantData.location}</span>
+        </div>
+        <div className="mt-4">
+          <a
+            href={restrurantData.geolocation}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+            <div className="inline-flex items-center gap-2  text-black py-2 px-4 rounded-lg border-2 border-red-400">
+            <FaDirections className="text-xl" />
+           <span className="">Direction</span>
+           </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RestrurantData;
