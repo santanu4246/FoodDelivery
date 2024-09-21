4185b1bb298b5cccc5003809f61286ceac2eaeb2
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-21 12:41:23 +0530

    cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,8 +1,31 @@
 import React, { useEffect } from "react";
 import { UserAuth } from "../../store/UserAuth";
+import { IoLocationSharp } from "react-icons/io5";
 
-function RestuCard({ item }) {
-  return <div className=""></div>;
+function RestuCard({ item,count }) {
+  return (
+    <div className="bg-white flex w-[70%] border border-gray-200 rounded-lg shadow-sm overflow-hidden m-2">
+      <img
+        src={item.image}
+        alt={item.name}
+        className="w-[200px] h-[100px] object-cover"
+      />
+      <div className="p-2">
+        <h2 className="text-lg font-semibold text-gray-800 truncate">
+          {item.name}
+        </h2>
+        <div className="flex items-center gap-[2px]">
+          <IoLocationSharp className="text-[15px] relative top-[1px]" />
+          <p className="text-gray-600 text-sm font-[500] truncate">
+            {item.location}
+          </p>
+        </div>
+        <p className="text-[13px] font-semibold text-gray-800 truncate">
+          {count} Foods in cart
+        </p>
+      </div>
+    </div>
+  );
 }
 
 function Cart() {
@@ -10,11 +33,14 @@ function Cart() {
   useEffect(() => {
     getCart();
   }, []);
-  useEffect(() => {
-    console.log(cart);
-  }, [cart]);
-
-  return <div className=""></div>;
+  return (
+    <div className="flex flex-col items-center">
+      {cart?.items?.map((item) => {
+        console.log(item);
+        return <RestuCard item={item.restaurant} count={item.foods.length} key={item._id} />;
+      })}
+    </div>
+  );
 }
 
 export default Cart;
