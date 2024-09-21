c91ac3f3039f92b5da2448f522257dbe2a994980
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-21 12:48:41 +0530

    cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -2,27 +2,34 @@ import React, { useEffect } from "react";
 import { UserAuth } from "../../store/UserAuth";
 import { IoLocationSharp } from "react-icons/io5";
 
-function RestuCard({ item,count }) {
+function RestuCard({ item, count }) {
   return (
-    <div className="bg-white flex w-[70%] border border-gray-200 rounded-lg shadow-sm overflow-hidden m-2">
-      <img
-        src={item.image}
-        alt={item.name}
-        className="w-[200px] h-[100px] object-cover"
-      />
-      <div className="p-2">
-        <h2 className="text-lg font-semibold text-gray-800 truncate">
-          {item.name}
-        </h2>
-        <div className="flex items-center gap-[2px]">
-          <IoLocationSharp className="text-[15px] relative top-[1px]" />
-          <p className="text-gray-600 text-sm font-[500] truncate">
-            {item.location}
+    <div className="bg-white items-center justify-between flex w-[70%] border border-gray-200 rounded-lg shadow-sm overflow-hidden m-2">
+      <div className="flex">
+        <img
+          src={item.image}
+          alt={item.name}
+          className="w-[200px] h-[100px] object-cover"
+        />
+        <div className="p-2">
+          <h2 className="text-lg font-semibold text-gray-800 truncate">
+            {item.name}
+          </h2>
+          <div className="flex items-center gap-[2px]">
+            <IoLocationSharp className="text-[15px] relative top-[1px]" />
+            <p className="text-gray-600 text-sm font-[500] truncate">
+              {item.location}
+            </p>
+          </div>
+          <p className="text-[13px] font-semibold text-gray-800 truncate">
+            {count} Foods in cart
           </p>
         </div>
-        <p className="text-[13px] font-semibold text-gray-800 truncate">
-          {count} Foods in cart
-        </p>
+      </div>
+      <div className="">
+        <button className="mr-[20px] rounded-[7px] text-[white] font-[500] py-[5px] px-[2rem] bg-[#ff008c]">
+          Proceed payment
+        </button>
       </div>
     </div>
   );
@@ -34,10 +41,16 @@ function Cart() {
     getCart();
   }, []);
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center my-[2rem]" >
       {cart?.items?.map((item) => {
         console.log(item);
-        return <RestuCard item={item.restaurant} count={item.foods.length} key={item._id} />;
+        return (
+          <RestuCard
+            item={item.restaurant}
+            count={item.foods.length}
+            key={item._id}
+          />
+        );
       })}
     </div>
   );
