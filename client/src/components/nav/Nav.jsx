b020e91d085995ae91bab3364a4d86faa0fe6970
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-10-27 23:39:52 +0530

    Update Nav.jsx

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -255,7 +255,7 @@ const Nav = () => {
             aria-label="Toggle location"
           >
             <CiLocationOn className="text-2xl" />
-            <span className="ml-2 font-medium">{location.slice(1, -1)}</span>
+            <span className="ml-2 font-medium">{location}</span>
           </div>
 
           {/* User Profile */}
