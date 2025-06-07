831c0eaea1c1e51b4376fe47404a1b5e2a9fa186
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2025-06-07 19:18:08 +0530

    Remove admin authentication requirement from addfood-to-database route in FoodRoutes.js to allow image uploads without admin checks.

--- server/routes/FoodRoutes.js
+++ server/routes/FoodRoutes.js
@@ -5,7 +5,7 @@ import AuthAdmin from "../middleware/AuthAdmin.js";
 
 const FoodRouter = express.Router()
 FoodRouter.post('/addfood/:id', AuthAdmin, addFood);  
-FoodRouter.post('/addfood-to-database', AuthAdmin, uploadStorage.single('image'), AddFoodToDatabase);  
+FoodRouter.post('/addfood-to-database', uploadStorage.single('image'), AddFoodToDatabase);  
 FoodRouter.put('/menu/:foodid', AuthAdmin, updateFood);  
 FoodRouter.delete('/menu-delete/:foodid', AuthAdmin, deleteFood); 
 FoodRouter.get('/get-food-by-menu-id/:menuid', getFoodByMenuId); 
